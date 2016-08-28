2dd55edc8e7f66b8d797e763afb4c887bdaacd1b
sara <sara@localhost>
2016-08-28 00:08:30 -0500

    practica 2 modificacion clase app

--- app/src/app/App.java
+++ app/src/app/App.java
@@ -5,8 +5,9 @@ public class App {
     public static void main(String[] args) {
         
         movil movil = new movil("Samsung", "Dorado");
-        //movil.llamar();
         movil.llamadaEspecial(movil);
+        movil movil2 = new movil("LG", "Negro");
+        movil2.llamar();
         
     }
     
